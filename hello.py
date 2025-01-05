d92fa6f0e0e7860cc3931599b84c18eab92347a7
Ariadne Araujo <ariadne.araujo123@gmail.com>
2025-01-05 16:35:53 -0300

    tipos de dados

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print('aqui testando esse negócioksks')
+#Testando tipos de dados
+
+print(123456) #int
+print(123.456) #float
+print('aqui testando esse negócio') #string ''
+print("aqui testando esse negócio") #string ""
+print(10 == 10) # booleano
+print(10 == 11) # booleano
+print(type(10 == 10)) #descobrir tipos de dados
+
+print(1+1)
+print('a'+'b')
+
+print(int('1'), type(int('1'))) #conversao de dados
+print(float('1.2')+ 1) #conversao de dados
