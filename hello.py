dfcc167120e9d8be01a0f2776d74c753e7e98a8a
Ariadne Araujo <ariadne.araujo123@gmail.com>
2025-01-05 17:32:25 -0300

    variaveis

--- hello.py
+++ hello.py
@@ -13,3 +13,21 @@ print('a'+'b')
 
 print(int('1'), type(int('1'))) #conversao de dados
 print(float('1.2')+ 1) #conversao de dados
+
+meu_nickname = 'itsariadnea'
+print(meu_nickname)
+
+nome = 'ariadne'
+sobrenome = 'araujo'
+idade = 20
+ano_de_nascimento = 2024-idade
+maior_de_idade = idade >= 18
+altura_metros = 1.57
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_de_nascimento)
+print('É maior de idade:', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
